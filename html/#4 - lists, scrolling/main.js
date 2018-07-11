b7f6d6223c7cbcfbe6795c9305302aa54c81bd58
kjano10 <kuba.jano3@gmail.com>
2018-07-11 14:22:57 +0200

    scrollUp button added

--- html/#4 - lists, scrolling/main.js
+++ html/#4 - lists, scrolling/main.js
@@ -7,4 +7,18 @@ $(function(){
     $('#link1').click(function(){
         my_scroll("#para1");
     });
+
+    $('#up').click(function(e){
+        e.preventDefault();
+        my_scroll("#logo");
+    });
+
+    $(window).scroll(function(){
+        if($(this).scrollTop()>300)
+            $('#scrollUp').fadeIn();
+        else
+            $('#scrollUp').fadeOut();   
+    });
+
 });
+
